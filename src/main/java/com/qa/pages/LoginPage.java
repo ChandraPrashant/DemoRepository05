f22dbffc489d485dae3a3bbf1a5e3418ca21eb64
Prashant Chandra <prashant.chandra@happiestminds.com>
2020-03-29 21:53:12 +0530

    Code Commited in Repo

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,61 @@
+package com.qa.pages;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+
+	@BeforeSuite
+	public void openSystem() {
+		System.out.println("openSystem");
+	}
+
+	@BeforeTest
+	public void lunchBrowser() {
+		System.out.println("lunchBrowser");
+	}
+
+	@BeforeClass
+	public void EnterURL() {
+		System.out.println("EnterURL");
+	}
+
+	@BeforeMethod
+	public void Login() {
+		System.out.println("Login");
+	}
+
+	@Test
+	public void compose() {
+		System.out.println("Login Compose");
+	}
+
+
+	@AfterSuite
+	public void shutdown() {
+		System.out.println("shutdown");
+	}
+
+	@AfterTest
+	public void CloseBrowser() {
+		System.out.println("CloseBrowser");
+	}
+
+	@AfterClass
+	public void closeTab() {
+		System.out.println("LoginPagecloseTab");
+	}
+
+	@AfterMethod
+	public void logout() {
+		System.out.println("LoginPagelogout");
+	}
+
+}
