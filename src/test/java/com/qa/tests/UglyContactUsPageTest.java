498519243f69fa536a895c5743427fc143d7f0ab
Prashant.chandra <Prashant.chandra@HMECL003091.happiestminds.com>
2020-11-17 15:51:37 +0530

    Removed code

--- src/test/java/com/qa/tests/UglyContactUsPageTest.java
+++ src/test/java/com/qa/tests/UglyContactUsPageTest.java
@@ -22,9 +22,9 @@ public class UglyContactUsPageTest extends ReadPropertyFile {
 
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + prop.getProperty("driverpath"));
 		// WebDriverManager.chromedriver().setup();
-		System.out.println("25 line");
+		
 		ReadExcelData reader = new ReadExcelData(System.getProperty("user.dir") + prop.getProperty("filepath"));
-		System.out.println("25 line");
+		
 		int rowCount = reader.getRowCount(prop.getProperty("uglysheetname"));
 		
 		for (int rowNum = 2; rowNum <= rowCount; rowNum++) {
